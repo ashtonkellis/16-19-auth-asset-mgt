e59aa51b3be6c20d3070a5906360a945b21753d1
Ashton K Ellis <ashtonkellis@gmail.com>
2018-07-02 17:51:43 -0700

    initial tests passing

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -1,3 +1,5 @@
 process.env.NODE_ENV = 'development';
 process.env.PORT = 5000;
 process.env.MONGODB_URI = 'mongodb://localhost/testing';
+
+process.env.SALT = 'SoooooSalty!SoMuchNaCLItIsRidiculous!';
