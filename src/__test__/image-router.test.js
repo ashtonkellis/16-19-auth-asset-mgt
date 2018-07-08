9390210fe6b24702991b7b5c6dd18a8792324128
Ashton K Ellis <ashtonkellis@gmail.com>
2018-07-08 01:30:45 -0700

    delete image route added, tests passing

--- src/__test__/image-router.test.js
+++ src/__test__/image-router.test.js
@@ -45,16 +45,44 @@ describe('TESTING ROUTES AT /api/images', () => {
           .set('Authorization', `Bearer ${token}`)
           .field('title', 'poop emoji')
           .attach('image', poopImg);
+
         expect(response.status).toEqual(200);
         expect(response.body.title).toEqual('poop emoji');
         expect(response.body._id).toBeTruthy();
         expect(response.body.url).toBeTruthy();
         expect(response.body.url).toBeTruthy();
-      } catch (err) { 
+      } catch (err) {
         // console.log(err, 'ERROR FROM IMAGE-ROUTER TEST: post 200 test'); /*eslint-disable-line*/
         expect(err).toEqual('foo');
       }
       return undefined;
+    });
+
+    test('POST 400 on bad request', async () => {
+      try {
+        const response = await superagent.post(apiUrl)
+          .set('Authorization', `Bearer ${token}`)
+          .field('BADFIELDNAME', 'BADFIELD')
+          .attach('image', poopImg);
+
+        expect(response).toBe('foo');
+      } catch (err) {
+        expect(err.status).toBe(400);
+      }
+    });
+
+    test('POST 401 on bad token', async () => {
+      try {
+        const badToken = 'BADTOKEN';
+        const response = await superagent.post(apiUrl)
+          .set('Authorization', `Bearer ${badToken}`)
+          .field('BADFIELDNAME', 'BADFIELD')
+          .attach('image', poopImg);
+
+        expect(response).toBe('foo');
+      } catch (err) {
+        expect(err.status).toBe(401);
+      }
     });
   });
 
@@ -71,6 +99,62 @@ describe('TESTING ROUTES AT /api/images', () => {
       } catch (err) {
         console.log(err, 'ERROR FROM IMAGE-ROUTER-TEST: get 200 test'); /*eslint-disable-line*/
         expect(err).toEqual('FAILING IN GET 200 POST');
+      }
+    });
+
+    test('404 GET due to bad id', async () => {
+      try {
+        const response = await superagent.get(`${apiUrl}/${'BADID'}`)
+          .set('Authorization', `Bearer ${token}`);
+
+        expect(response).toBe('foo');
+      } catch (err) {
+        expect(err.status).toBe(404);
+      }
+    });
+
+    test('401 GET due to bad token', async () => {
+      try {
+        const response = await superagent.get(`${apiUrl}/${image._id}`)
+          .set('Authorization', `Bearer ${'BADTOKEN'}`);
+        expect(response).toEqual('foo');
+      } catch (err) {
+        expect(err.status).toBe(401);
+      }
+    });
+  });
+
+  describe('DELETE ROUTES to /api/images', () => {
+    test('204 DELETE /api/images for succesful delete', async () => {
+      try {
+        const response = await superagent.delete(`${apiUrl}/${image._id}`)
+          .set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toEqual(204);
+      } catch (err) {
+        expect(err).toEqual('FAILING IN DELETE 204 POST');
+      }
+    });
+
+    test('404 DELETE for bad id', async () => {
+      try {
+        const response = await superagent.delete(`${apiUrl}/${'BADID'}`)
+          .set('Authorization', `Bearer ${token}`);
+
+        expect(response).toEqual('FAILING IN DELETE 404');
+      } catch (err) {
+        expect(err.status).toBe(404);
+      }
+    });
+
+    test('401 DELETE for bad token', async () => {
+      try {
+        const response = await superagent.delete(`${apiUrl}/${image._id}`)
+          .set('Authorization', `Bearer ${'BADTOKEN'}`);
+
+        expect(response).toEqual('FAILING IN DELETE 401');
+      } catch (err) {
+        expect(err.status).toBe(401);
       }
     });
   });
